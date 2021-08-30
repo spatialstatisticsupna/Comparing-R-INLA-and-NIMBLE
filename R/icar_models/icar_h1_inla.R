5ed714665f37cd6dfd6a6483fcee0608936d87ba
ArantxaUrdangarin <42638250+ArantxaUrdangarin@users.noreply.github.com>
2021-08-30 09:02:17 +0200

    Update icar_h1_inla.R

--- R/icar_models/icar_h1_inla.R
+++ R/icar_models/icar_h1_inla.R
@@ -5,30 +5,28 @@
 
 
 
-#############################
-##  ICAR - RW1 - Type I    ##
-#############################
-
+###########################
+##  ICAR - RW1 - Type I  ##
+###########################
 icar.type1.h1 <- O ~ 1 + f(ID.area, model="besag", graph=Rs, scale.model=FALSE, 
-                            constr=TRUE, hyper=list(prec=list(prior=sdunif))) + 
+                           constr=TRUE, hyper=list(prec=list(prior=sdunif))) + 
                          f(ID.year, model="rw1", constr=TRUE, 
-                            hyper=list(prec=list(prior=sdunif))) +
+                           hyper=list(prec=list(prior=sdunif))) +
                          f(ID.area.year, model="iid", constr=TRUE, 
-                            hyper=list(prec=list(prior=sdunif)))
+                           hyper=list(prec=list(prior=sdunif)))
 
 
 
 
-##############################################
-##  ICAR - RW1 - Type II/Type III/Type IV   ##
-##############################################
-
+#############################################
+##  ICAR - RW1 - Type II/Type III/Type IV  ##
+#############################################
 icar.types.h1 <- O ~ 1 + f(ID.area, model="besag", graph=Rs, scale.model=FALSE, 
-                            constr=TRUE, hyper=list(prec=list(prior=sdunif))) + 
+                           constr=TRUE, hyper=list(prec=list(prior=sdunif))) + 
                          f(ID.year, model="rw1", constr=TRUE,
-                            hyper=list(prec=list(prior=sdunif))) +
+                           hyper=list(prec=list(prior=sdunif))) +
                          f(ID.area.year, model="generic0", Cmatrix=R, 
-                            rankdef=rank.def, constr=TRUE,
-                            extraconstr=list(A=A.constr, e=e), 
-                            hyper=list(prec=list(prior=sdunif)))
+                           rankdef=rank.def, constr=TRUE,
+                           extraconstr=list(A=A.constr, e=e), 
+                           hyper=list(prec=list(prior=sdunif)))
 
